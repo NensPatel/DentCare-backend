990facdb193c26da4740cb4a349344186f48329b
Nens Patel <patelnens512@gmail.com>
2024-10-16 22:04:41 +0530

    added createCounter api

--- controllers/counter.controller.js
+++ controllers/counter.controller.js
@@ -1,20 +1,48 @@
-const counterSchema = require('../models/counter.model');
+const counterSchema = require("../models/counter.model");
+
+const logError = (error) => {
+  console.error("Error: ", error);
+};
+
+exports.createCounter = async (req, res) => {
+  const { count, title, subTitle } = req.body;
+
+  try {
+    const counter = new counterSchema({
+      count,
+      title,
+      subTitle,
+    });
+    const data = await counter.save();
+    return res.status(200).send({
+      data,
+      message: "Counter created successfully",
+      isSuccess: true,
+    });
+  } catch (error) {
+    logError(error);
+    return res.status(500).send({
+      message: "Failed to create Counter due to server error",
+      isSuccess: false,
+    });
+  }
+};
 
 exports.getCounter = async (req, res) => {
-    try {
-        const getData = await counterSchema.find();
-        const counters = await counterSchema.countDocuments();
-        res.status(200).json({
-            data: getData,
-            totalCounters: counters,
-            message: 'Counter Retrieved Successfully',
-            isSuccess: true
-        });
-    } catch (error) {
-        res.status(500).json({
-            error,
-            message: 'Failed to Retrieve Counter',
-            isSuccess: false
-        });
-    }
-}
+  try {
+    const getData = await counterSchema.find();
+    const counters = await counterSchema.countDocuments();
+    res.status(200).json({
+      data: getData,
+      totalCounters: counters,
+      message: "Counter Retrieved Successfully",
+      isSuccess: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "Failed to Retrieve Counter",
+      isSuccess: false,
+    });
+  }
+};
--- routes/counter.route.js
+++ routes/counter.route.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const getCounter = require('../controllers/counter.controller');
+const createCounter = require("../controllers/counter.controller");
+const getCounter = require("../controllers/counter.controller");
 
-router.get('/getCounter', getCounter.getCounter);
+router.post("/createCounter", createCounter.createCounter);
+router.get("/getCounter", getCounter.getCounter);
 
 module.exports = router;
